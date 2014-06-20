e73b6ece28710add9c8b0b7b68d841540372e0a3
Tomasz Dziuda <dziudek@gmail.com>
2014-06-21 01:07:15 +0200

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,97 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+        // move main image to header
+        if(
+        	$('.post-template').length > 0 || 
+        	$('.page-template').length > 0
+        ) {
+        	var featured_image = $('img[alt="featured-image"]');
+        	var featured_video = $('.post__content iframe:first-child')
+        	// check if the featured image exists
+        	if(featured_image && featured_image.length > 0) {
+        		// create container for the image
+        		featured_image.appendTo($('.post__media'));
+        	} else if(featured_video && featured_video.length > 0) {
+        		featured_video.appendTo($('.post__media'));
+        	}
+        }
+
+        if(
+        	$(document.body).hasClass('home-template') ||
+        	$(document.body).hasClass('archive-template') ||
+        	$(document.body).hasClass('tag-template')
+        ) {
+          // get the post images
+          var blocks = [];
+          
+          $('.post__wrapper_helper--notloaded').each(function(i, block) {
+          	blocks.push(block);
+          });
+          
+          var add_class = function(block, class_name, delay) {
+          	setTimeout(function() {
+          		$(block).addClass(class_name);
+          	}, delay);
+          };
+          
+          for(var i = 0; i < blocks.length; i++) {
+          	add_class(blocks[i], 'post__wrapper_helper--animated', i * 200);
+          }
+          
+          $('.post__wrapper_helper--notloaded').each(function(i, wrapper) {
+            wrapper = $(wrapper);
+            var img = wrapper.find('p > img')[0];
+            if(img) {
+              // wait for the images
+              var timer = setInterval(function() {
+                // when the image is laoded
+                if(img.complete) {
+                  // stop periodical calls
+                  clearInterval(timer);
+                  // generate the image wrapper
+                  var src = $(img).attr('src');
+                  img.remove();
+                  var img_container = $('<div class="post__image el__transition_long" style="background-image: url(\''+src+'\')"></div>');
+                  img_container.appendTo(wrapper);
+                  wrapper.removeClass('post__wrapper_helper--notloaded');
+                  // add class with delay
+                  setTimeout(function() {
+                    img_container.addClass('post__image--loaded');
+                  }, 250);
+                }          
+              }, 500);
+              // add necessary mouse events
+              wrapper.mouseenter(function() {
+                wrapper.addClass('post__wrapper_helper--hover');
+              });
+
+              wrapper.mouseleave(function() {
+                wrapper.removeClass('post__wrapper_helper--hover');
+              });
+            } else {
+              // where there is no image - display the text directly
+              wrapper.addClass('post__wrapper_helper--hover');
+            }
+          });
+        }
+        // fit videos
+        $(".post-header").fitVids();
+        $(".post-content").fitVids();
+        // menu behaviour
+        var main_menu = $(".menu");
+        main_menu.click(function() {
+          if(main_menu.hasClass("menu--open")) {
+            main_menu.removeClass("menu--open");
+          } else {
+            main_menu.addClass("menu--open");
+          }
+        });
+    });
+}(jQuery));
